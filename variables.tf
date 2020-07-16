387b5e51573f7b903df8b505f6ed783e44f57f35
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-07-16 09:43:14 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ description = "Project name to manage"
 
 }
 
+variable "azuredevops_group" {
+description = "Group to manage"
+  default = "Build Administrators"
+
+}
+
 variable "user_principal_name" {
 description = "User principal name to add"
 
